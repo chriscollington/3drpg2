540597011d3bb6f88973b308531f6e660832479b
chris <540218@escg.ac.uk>
2024-12-12 12:32:22 +0000

    ;m,

--- Assets/scripts/PauseMenu.cs
+++ Assets/scripts/PauseMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject Pausemenu;
-    public GameObject OptionsMenu;
     public static bool isPaused;
     public AudioSource backgroundMusic;  // Reference to the AudioSource for background music
 
